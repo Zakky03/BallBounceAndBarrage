bb52be2a7318130445dc68eb30270fd2fff75689
DESKTOP-P97N7L7\tk_ka <tk_kakutasu@yahoo.co.jp>
2020-08-16 18:58:04 +0900

    タイトル作った

---
+++ 1WGJ_Prototype/Assets/BallBounceAndBarrage/Scripts/Koitan/TitleUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUIManager : MonoBehaviour
+{
+    [SerializeField]
+    Animator startAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayAnim()
+    {
+        startAnimator.enabled = true;
+        StartCoroutine(LoadCoroutine());
+    }
+
+    IEnumerator LoadCoroutine()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Main");
+    }
+}
